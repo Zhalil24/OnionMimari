42700a1127eb222d2900a3d184d1e115686d2034
Halil Zilif <93616680+Zhalil24@users.noreply.github.com>
2024-06-15 21:39:10 +0300

    Entitiyler oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Commen;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail()
+        {
+            
+        }
+        public Detail(string title, string description, int categoryId  )
+        {
+            Title = title;
+            Descriptipn = description;
+            CategoryId = categoryId;
+            
+        }
+        public required string Title { get; set; }
+        public required string Descriptipn { get; set; }
+
+        public required int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
